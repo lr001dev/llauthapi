3202293441d28ab597a1e95abfdfab1d9b57b014
Luis Rosario <luisrosariomix@gmail.com>
2019-10-09 12:57:01 -0700

    Added custom routes for login and cookie checks

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  resources :users
+  root 'welcome#index'
+  resources :users do
+    collection do
+      post '/login', to: 'users#login'
+    end
+    collection do
+      get '/check', to: 'users#check'
+    end
+    collection do
+      delete '/destroyCookie', to: 'users#destroyCookie'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

0a06cea1b247eb071e95c9c15d071144cdbbbc6c
Luis Rosario <luisrosariomix@gmail.com>
2019-10-09 13:38:11 -0700

    Updated users_controller and added support for rest routes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,22 +3,41 @@ class UsersController < ApplicationController
 
   # GET /users
   def index
-    @users = User.all
-
+    @users = User.select('first_name, last_name, email, id').all
     render json: @users
   end
+
+  def check
+    if get_current_user.id
+      render json: { user: get_current_user.id }
+    end
+
+    def destroyCookie
+      cookie.delete(:jwt)
+    end
 
   # GET /users/1
   def show
     render json: @user
   end
 
+  def login
+    user = User.find_by(email: params[:user][:email])
+    puts user
+    if user && user.authenticate(params[:user][:password])
+      token = create_token(user.id, user.email)
+      cookies.signed[:jwt] = { value: token, httponly: true }
+      render json: { status: 200, token: token, user: user }
+    else
+      render json: { status 401, message: "Unauthorized" }
+    end
+
   # POST /users
   def create
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: 201
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -41,11 +60,26 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
+      @user = User.select('first_name, last_name, email, username, id').find(params[:id])
+    end
+
+    def set_user_update
       @user = User.find(params[:id])
+    end
+    def payload(id, username)
+      {
+        exp: (Time.now + 30.minutes).to_i,
+        iat: Time.now.to_i,
+        iss: ENV['JWT_ISSUER'],
+        user: {
+          id: id,
+          email: email
+        }
+      }
     end
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password_digest)
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
     end
 end
